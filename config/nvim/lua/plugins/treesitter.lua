91c3ba925ac693e28acaa689bbb8bdff5d506bf1
SydX-pages <xusyd1222@gmail.com>
2026-01-18 17:47:08 +0800

    waybar_vpn

--- config/nvim/lua/plugins/treesitter.lua
+++ config/nvim/lua/plugins/treesitter.lua
@@ -1,16 +1,28 @@
-return{
-    {
-        "nvim-treesitter/nvim-treesitter",
-        build = ":TSUpdate",
-        config = function () 
-            local configs = require("nvim-treesitter.configs")
-            configs.setup({
-                auto_install = true,
-                ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "elixir", "heex", "javascript", "html", "java", "python" },
-                sync_install = false,
-                highlight = { enable = true },
-                indent = { enable = true },  
-            })
-        end
-    }
+return {
+	{
+		"nvim-treesitter/nvim-treesitter",
+		build = ":TSUpdate",
+		config = function()
+			local configs = require("nvim-treesitter.configs")
+			configs.setup({
+				auto_install = true,
+				ensure_installed = {
+					"c",
+					"lua",
+					"vim",
+					"vimdoc",
+					"query",
+					"elixir",
+					"heex",
+					"javascript",
+					"html",
+					"java",
+					"python",
+				},
+				sync_install = false,
+				highlight = { enable = true },
+				indent = { enable = true },
+			})
+		end,
+	},
 }
